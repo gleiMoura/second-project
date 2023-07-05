b707afc64f0327792d717a33642a1c05c11a9f3c
gleiMoura <gle.bar.mou@gmail.com>
2023-07-05 16:19:38 -0300

    :sparkles: Creates a PostType

--- src/template/Home.tsx
+++ src/template/Home.tsx
@@ -1,63 +1,17 @@
-import { MouseEventHandler, useState, memo, useCallback } from "react";
-
-const Button = memo(({ increaseValue, name }: { increaseValue: MouseEventHandler, name: string }) => {
-    console.log("filho renderizou")
-    return (
-        <button
-            onClick={increaseValue}
-            style={{
-                background: "red",
-                width: "100px",
-                height: "50px",
-                color: "#fff",
-                fontSize: "30px"
-            }}
-        >
-            {name}
-        </button>
-    )
-}
-)
-
+// import UsingCallback from "../components/UsingCallBack";
+import UsingMemo from "../components/UsingMemo";
 const Home = () => {
-    const [counter, setCounter] = useState<number>(0);
-    console.log("pai renderizou")
-
-    const handleChangeCounter = useCallback(() => {
-        setCounter((c) => c + 1);
-    }, []);
-
-    const handleClearCounter = useCallback(() => {
-        setCounter(0)
-    }, []); 
-    
     return (
         <div style={{
             width: "100wh",
-            height: "100vh",
+            height: "auto",
             background: "#888",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
         }}>
-            <div style={{
-                background: '#fff',
-                width: "1000px",
-                height: "500px",
-                marginTop: "200px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <header>
-                    <p style={{ fontSize: "50px" }}>contador: {counter}</p>
-                </header>
-                <section>
-                    <Button increaseValue={handleChangeCounter} name="Plus 1" />
-                    <Button increaseValue={handleClearCounter} name="Clear" />
-                </section>
-            </div>
+            {/* <UsingCallback /> */}
+            <UsingMemo />
         </div>
     )
 };
